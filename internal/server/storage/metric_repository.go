87d3d9993370e63446ea98d2ab1f4bc50ba58d27
d.cherenkov <Qwer19966991>
2022-01-24 11:54:37 +0300

    increment-11: add metric

--- internal/server/storage/metric_repository.go
+++ internal/server/storage/metric_repository.go
@@ -82,7 +82,8 @@ func NewMetricRepository(ctx context.Context, wg *sync.WaitGroup, cfg server.Con
 }
 
 func (r *MetricRepositoryImpl) notifyUpdateDBValue(ctx context.Context) {
-	if r.store != nil && r.cfg.StoreInterval == 0 {
+	//if r.store != nil && r.cfg.StoreInterval == 0 {
+	if r.store != nil {
 		r.store.SaveDBValue(ctx)
 	}
 }
